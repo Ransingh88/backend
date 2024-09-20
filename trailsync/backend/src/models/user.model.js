103d41075983343078728842f316585dace4c277
dransingh <rajaransingh@gmail.com>
2024-09-21 00:11:26 +0530

    feat: utility added error, res

---
+++ trailsync/backend/src/models/user.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      index: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["guest", "rider", "admin"],
+      default: "guest",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = mongoose.model("User", userSchema);
