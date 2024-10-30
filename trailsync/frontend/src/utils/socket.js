9c388e1f5e256adcf0bf94654a48c85e69c663b1
debasishransingh <rajaransingh@gmail.com>
2024-10-30 07:43:20 +0530

    fix: env variable added

--- trailsync/frontend/src/utils/socket.js
+++ trailsync/frontend/src/utils/socket.js
@@ -1,4 +1,5 @@
 import { io } from "socket.io-client";
-const URL = "http://localhost:8000";
+
+const URL = import.meta.env.VITE_API_SOCKET_URL || "http://localhost:8000";
 
 export const socket = io(URL);
